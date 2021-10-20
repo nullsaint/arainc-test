5e3b99427cbbaca1d14c56511f349e3fd11b8e47
MD Farhanul Haque <nullsaint00@gmail.com>
2021-10-20 12:44:28 +0600

    Production changes

--- arainc/settings/production.py
+++ arainc/settings/production.py
@@ -3,13 +3,20 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['iog.araincbd.com']
+ALLOWED_HOSTS = ['app.araincbd.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-	'OPTIONS': {
-	'read_default_file':'/home/ubuntu/arainc/auth/mysql.cnf'
-	},
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+# 	'OPTIONS': {
+# 	'read_default_file':'/home/ubuntu/arainc/auth/mysql.cnf'
+# 	},
+#     }
+# }
--- arainc/wsgi.py
+++ arainc/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arainc.settings.development')
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arainc.settings.production')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arainc.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arainc.settings.production')
 
 application = get_wsgi_application()
